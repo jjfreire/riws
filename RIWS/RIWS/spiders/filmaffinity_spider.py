ce915808d8915c0b9071ac7d2c363ba1f839d355
AlejandroArizaUDC <alejandro.ariza@udc.es>
2024-10-19 00:33:15 +0200

    fa_spider second page

--- RIWS/RIWS/spiders/filmaffinity_spider.py
+++ RIWS/RIWS/spiders/filmaffinity_spider.py
@@ -3,7 +3,8 @@ from RIWS.items import FAFilmItem
 
 class FilmaffinitySpider(scrapy.Spider):
     name = "fa_spider"
-    base_url = 'https://www.filmaffinity.com/es/category.php?id=2024films'
+    start_page = 1
+    base_url = 'https://www.filmaffinity.com/es/category.php?id=2024films&page={}'.format(start_page)
     start_urls = [
         base_url
     ]
@@ -17,6 +18,15 @@ class FilmaffinitySpider(scrapy.Spider):
             item['url'] = response.urljoin(film_url)  # Create absolute URL
             # Pass the item to the next request and call the parse_film method
             yield response.follow(film_url, callback=self.parse_film, meta={'item': item})
+
+        current_page = int(response.url.split('page=')[-1].split('&')[0])
+        max_pages = 2
+
+        if current_page < max_pages:
+            next_page = current_page + 1
+            next_page_url = f'https://www.filmaffinity.com/es/category.php?id=2024films&page={next_page}'
+            # Realizar la solicitud de la siguiente página
+            yield scrapy.Request(next_page_url, callback=self.parse)
 
     def parse_film(self, response):
         # Extract the previously passed item
