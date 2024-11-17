a9ce830c3586e1e75f995d6249a92933120d9dd9
juanjo <jjfreire@allenta.com>
2024-11-17 21:10:57 +0100

    add docker infrastructure

---
+++ run/index-initializer/initialize_index.py
@@ -0,0 +1,80 @@
+import json, os
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(hosts=["http://elasticsearch:9200"])
+index_name = "jobs"
+
+mapping = {
+    "mappings": {
+        "properties": {
+            "title": {"type": "text"},
+            "company": {
+                "type": "text",
+                "fields": {
+                    "keyword": {
+                        "type": "keyword"
+                    }
+                }
+            },
+            "description": {"type": "text"},
+            "link": {"type": "text"},
+            "min_salary": {"type": "long"},
+            "max_salary": {"type": "long"},
+            "duration": {
+                "type": "text",
+                "fields": {
+                    "keyword": {
+                        "type": "keyword"
+                    }
+                }
+            },
+            "workday": {
+                "type": "text",
+                "fields":{
+                    "keyword": {
+                        "type": "keyword"
+                    }
+                }
+            },
+            "location": {
+                "type": "text",
+                "fields": {
+                    "keyword": {
+                        "type": "keyword"
+                    }
+                }
+            },
+            "modality": {
+                "type": "text",
+                "fields": {
+                    "keyword": {
+                        "type": "keyword"
+                    }
+                }
+            },
+        }
+    }
+}
+
+def create_index():
+    es.indices.create(index=index_name, body=mapping)
+    print(f"Index '{index_name}' created successfully.")
+
+if not es.indices.exists(index=index_name):
+    create_index()
+else:
+    es.indices.delete(index=index_name)
+    create_index()
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(script_dir, 'jobs.json'), 'r') as f:
+    jobs = json.load(f)
+
+for i, job in enumerate(jobs):
+    doc_id = i + 1
+    if not es.exists(index=index_name, id=doc_id):
+        es.index(index=index_name, id=doc_id, body=job)
+        print(f"Document {doc_id} inserted.")
+
+print("Document insertion finished. Access http://localhost:3000")
